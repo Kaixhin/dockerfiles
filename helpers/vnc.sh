22129b818134c321ab76c1d5597394e7e55c45c4
Kaixhin <design@kaixhin.com>
2015-08-28 15:49:31 +0100

    Attempt to improve VNC helper

--- helpers/vnc.sh
+++ helpers/vnc.sh
@@ -10,5 +10,11 @@ apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y \
 export USER=root
 # Set password
 vncpasswd
+# Start VNC server to create xstartup
+vncserver :1
+# Stop VNC server
+vncserver -kill :1
+# Add LXDE destkop to xstartup
+echo "startlxde &" >> /root/.vnc/xstartup
 # Start VNC server
 vncserver :1 -geometry 1280x800 -depth 24 
