1ab67a84b90518a93ac204bc8e2d3a5c236978f1
Kaixhin <design@kaixhin.com>
2015-09-04 17:45:26 +0100

    Delete ceph

--- ceph/mon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Populate monitor daemon with monitor map and keyring
-ceph-mon --mkfs -i `hostname -s` --monmap /tmp/monmap --keyring /etc/ceph/ceph.mon.keyring
-# Mark monitor as created
-touch /var/lib/ceph/mon/ceph-`hostname -s`/done
-# Start monitor
-start ceph-mon id=`hostname -s`
--- ceph/monmap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Generate monitor map
-monmaptool --create --add `hostname -s` $MON_IP --fsid $UUID /tmp/monmap
-# Create default data directory on monitor host
-mkdir -p /var/lib/ceph/mon/ceph-`hostname -s`
