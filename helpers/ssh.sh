0f323083a306ce87e398ab369e488d3d5466ff37
Kaixhin <design@kaixhin.com>
2016-06-20 12:06:52 +0100

    Update sshd_config

--- helpers/ssh.sh
+++ helpers/ssh.sh
@@ -8,7 +8,7 @@ apt-get update && apt-get install -y openssh-server
 # Set password
 mkdir /var/run/sshd && echo "root:$password" | chpasswd
 # Allow root login with password
-sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 # Prevent user being kicked off after login
 sed -i 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' /etc/pam.d/sshd
 # Restart SSH daemon
